59afa5f69616b3aa14cb6c7d61b433598a35edf9
Burhan <2019cs626@student.uet.edu.pk>
2022-02-26 15:37:26 +0500

    Added Middleware

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const http = require('http')
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-    console.log("api running")
+const app = express()
+
+const port = 5000
+
+// NEXT is a middleware
+// It holds execution before going on the another res
+// execeutes from top to bottom
+app.use('/', (req, res, next) => [
+    next()  // This transfers control to next res object
+])
+app.use('/', (req, res, next) => [
+    res.send("Api running")
+])
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 })
-
-server.listen(5000)
