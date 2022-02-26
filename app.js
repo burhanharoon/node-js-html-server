5b4d9431e0c344235797adfcfaf5519a1174802c
Burhan <2019cs626@student.uet.edu.pk>
2022-02-26 16:00:14 +0500

    Added body parser

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 const port = 5000
 
-// NEXT is a middleware
-// It holds execution before going on the another res
-// execeutes from top to bottom
-app.use('/', (req, res, next) => [
-    next()  // This transfers control to next res object
-])
-app.use('/', (req, res, next) => [
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`    <form action="/product" method="post">
+    <input type="text" name="title">
+    <button type="submit">
+        Submit
+    </button>
+</form>
+`)
+})
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body)
+    res.send(`<h1>/Product</h1>`)
+})
+
+app.use('/', (req, res, next) => {
     res.send("Api running")
-])
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
